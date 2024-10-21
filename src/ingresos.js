24c13dbc3245922050b10cfeba249d5ec57b7004
nathan95oks <nathanaelalba75@gmail.com>
2024-10-20 23:15:04 -0400

    Multiples ingresos al array sin duplicacion

--- src/ingresos.js
+++ src/ingresos.js
@@ -1,6 +1,12 @@
 let ingresos = [];
 
 function agregarIngreso(monto, fecha) {
+
+    const ingresoExistente = ingresos.find(ingreso => ingreso.monto === monto && ingreso.fecha ===fecha);
+    if(ingresoExistente){
+        return null;
+    }
+
     const nuevoIngreso = { monto, fecha };
     ingresos.push(nuevoIngreso);
     return nuevoIngreso;
--- src/ingresos.spec.js
+++ src/ingresos.spec.js
@@ -2,8 +2,12 @@ console.log('Ejecutando pruebas de ingresos.spec.js');
 
 const { agregarIngreso, ingresos } = require('./ingresos');
 
+beforeEach(()=>{
+ ingresos.length =0;
+});
+
 test('debe agregar un ingreso al array', () => {
-    ingresos.length = 0; 
+   
 
     const monto = 100;
     const fecha = '2024-10-01';
@@ -12,6 +16,32 @@ test('debe agregar un ingreso al array', () => {
     expect(ingreso).toEqual({ monto, fecha });
     expect(ingresos.length).toBe(1);
     expect(ingresos[0]).toEqual({ monto, fecha });
+});
+
+
+
+test('debe agregar multiples ingresos al array',()=>{
+     const ingreso1 = agregarIngreso(100,'2024-10-01');
+     const ingreso2 = agregarIngreso(300,'2024-10-08');
+     const ingreso3 = agregarIngreso(800,'2024-10-02');
+     expect(ingresos.length).toBe(3);
+     expect(ingresos).toEqual([
+        {monto:100 ,fecha:'2024-10-01'},
+        {monto:300 ,fecha:'2024-10-08'},
+        {monto:800 ,fecha:'2024-10-02'},
+
+     ]);
+
+});
+
+
+test('no debe permitir ingresos duplicados', () => {
+    const ingreso1 = agregarIngreso(100, '2024-10-01');
+    const ingreso2 = agregarIngreso(100, '2024-10-01'); 
+
+    expect(ingreso1).toEqual({ monto: 100, fecha: '2024-10-01' });
+    expect(ingreso2).toBe(null); 
+    expect(ingresos.length).toBe(1); 
 });
 
 test('test de ejemplo', () => {
