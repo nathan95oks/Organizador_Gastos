ce357f9a720a0b77af3a056594cc38f86ee0330a
jarPunk <jar_ax@hotmail.com>
2024-10-23 22:36:40 -0400

    cypress prueba 3 mostar mensaje si no hay gastos

--- cypress/e2e/gastosTotal.cy.js
+++ cypress/e2e/gastosTotal.cy.js
@@ -1,38 +1,29 @@
 describe('Gestión de Gastos y Total de Gastos', () => {
 
     beforeEach(() => {
-      // Visita la página antes de cada prueba
       cy.visit('/');
-    });
-  
-    it('Debe agregar un gasto y mostrarlo en la lista de gastos', () => {
-      // Ingresa un monto de gasto
+    }); 
+    it('Debe agregar un gasto y mostrarlo en la lista de gastos', () => {  
       cy.get('#gasto').type('100');
-  
-      // Selecciona una fecha
-      cy.get('#fechaGasto').type('2024-10-23');
-  
-      // Envía el formulario de gastos
-      cy.get('#gasto-form').submit();
-  
-      // Verifica que el gasto se haya agregado correctamente
+      cy.get('#fechaGasto').type('2024-10-23');  
+      cy.get('#gasto-form').submit(); 
       cy.get('#resultado-gastos')
         .should('contain', 'Monto: $100 - Fecha: 2024-10-23');
     });
-    it('Debe calcular el total de los gastos correctamente', () => {
-        // Agrega el primer gasto
+    it('Debe calcular el total de los gastos correctamente', () => { 
         cy.get('#gasto').type('100');
         cy.get('#fechaGasto').type('2024-10-23');
         cy.get('#gasto-form').submit();
-    
-        // Agrega otro gasto
         cy.get('#gasto').type('200');
         cy.get('#fechaGasto').type('2024-10-23');
         cy.get('#gasto-form').submit();
-    
-        // Verifica que el total de los gastos sea correcto
         cy.get('#total-gastos').should('contain', 'Total de Gastos: $300');
       });
-    
+      it('Debe mostrar un mensaje si no hay gastos registrados', () => {
+        localStorage.removeItem('gastos');
+        cy.reload();
+        cy.get('#resultado-gastos', { timeout: 10000 }).should('contain', 'No hay gastos registrados');
+        cy.get('#total-gastos', { timeout: 10000 }).should('contain', 'Total de Gastos: $0');
+      });
   });
   
