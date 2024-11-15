eec2120b01bb97f0ee3de5b438c043eb0cd31b96
Jherson <Jherson.chambi@ucb.edu.bo>
2024-11-15 00:30:20 -0400

    Deberia devolver false si el valor es invalido valor negato Pasa

--- cypress/e2e/MetaAhorro.cy.js
+++ cypress/e2e/MetaAhorro.cy.js
@@ -2,14 +2,16 @@
 describe ("Agregar meta de ahorro",()=>{
     it ("Verificar si agrega los datos correctamente",()=>{
         cy.visit('index.html');
-        cy.get("#meta").type("200");
+        // const valor=300;
+        cy.get("#meta").type('300');
         cy.get("#Meta-form").submit();
-        cy.get("#meta-div").should("contain", "200");
+        cy.get("#meta-div").should('contain', '300');
     });
     it("Verificar que no permita valores vacíos o nulos", () => {
+        
         cy.visit('index.html');
         cy.get("#Meta-form").submit();
-        cy.get("#meta-div").should("not.contain", "%");
+        cy.get("#meta-div").should('not.contain', '%');
         cy.on('window:alert', (str) => {
             expect(str).to.equal('Por favor, ingrese una meta de ahorro válida.');
         });
--- src/EditarMetaAhorro.spec.js
+++ src/EditarMetaAhorro.spec.js
@@ -4,5 +4,7 @@ describe("Editar la meta de Ahorro ",()=>{
    it("Deberia editar la meta si el valo es valido ",()=>{
         expect(editar(200)).toEqual(200);
    });
-
+   it("Deberia devolver false  si el valor es invalido valor negativo ",()=>{
+    expect(editar(-100)).toEqual(false);
+   });
 });
