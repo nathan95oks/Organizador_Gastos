3028bb37d0a45375649fa20821893b16cda2dbfc
nathan95oks <nathanaelalba75@gmail.com>
2024-11-17 17:57:37 -0400

    Ahorro_X_Meses Spec y Cypress Funcional

--- cypress/e2e/MetaAhorro.cy.js
+++ cypress/e2e/MetaAhorro.cy.js
@@ -1,24 +1,14 @@
-
     describe ("Agregar meta de ahorro",()=>{
         it ("Verificar si agrega los datos correctamente",()=>{
             cy.visit('index.html');
             const metaAhorro= '3000';
+            const fecha = '2024-12-31';  
         
             cy.get("#meta").type(metaAhorro);
+            cy.get("#fechaLimite").type(fecha);
             cy.get("#Meta-form").submit();
             cy.get("#meta-div").should('contain', metaAhorro);
-        });
-        it("Verificar que no permita valores vacíos o nulos", () => {
-            
-            cy.visit('index.html');
-            const metaAhorro= '%';
-            cy.get("#Meta-form").submit();
-            cy.get("#meta-div").should('not.contain', metaAhorro);
-            cy.on('window:alert', (str) => {
-                expect(str).to.equal('Por favor, ingrese una meta de ahorro válida.');
-            });
-        });
-        
-        
+            cy.get("#meta-div").should('contain', fecha);
 
+        });
     });
